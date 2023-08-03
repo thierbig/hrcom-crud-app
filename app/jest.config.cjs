eef0530923b5a8c9a5ea7d05e84cfe43fdca178d
Your Name <you@example.com>
2023-08-03 02:02:57 -0400

    refactor test

--- app/jest.config.cjs
+++ app/jest.config.cjs
@@ -7,6 +7,6 @@ module.exports = {
       '\\.(css|less)$': 'identity-obj-proxy',
       '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/fileMock.js',
     },
-    setupFilesAfterEnv: ['../setupTests.js'],
+    setupFilesAfterEnv: ['./setupTests.js'],
 
   };
--- app/tests/App.test.jsx
+++ app/tests/App.test.jsx
@@ -4,6 +4,12 @@ import '@testing-library/jest-dom/extend-expect';
 import App from '../src/app'; // Adjust the import path to your App component
 
 test('renders without crashing', () => {
-    const { getByText } = render(<App />);
+  // Create a div with the id 'root'
+  const root = document.createElement('div');
+  root.id = 'root';
+  document.body.appendChild(root);
+
+  // Now the 'root' element should exist, and you can render the App component
+  const { getByText } = render(<App />, { container: root });
     expect(getByText('HRCom')).toBeInTheDocument();
   });
